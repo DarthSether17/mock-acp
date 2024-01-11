aae7707ec5abdf58634e754caa72553654fe06ba
DarthSether17 <156138144+DarthSether17@users.noreply.github.com>
2024-01-11 16:28:38 -0600

    made tilemap, character, and rough physics.

--- main.ts
+++ main.ts
@@ -1,19 +1,30 @@
-scene.setBackgroundColor(2)
-let mySprite = sprites.create(img`
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 b c 5 5 d 4 c . . 
-    . b b b b 5 5 5 b f d d 4 4 4 b 
-    . b d 5 b 5 5 b c b 4 4 4 4 b . 
-    . . b 5 5 b 5 5 5 4 4 4 4 b . . 
-    . . b d 5 5 b 5 5 5 5 5 5 b . . 
-    . b d b 5 5 5 d 5 5 5 5 5 5 b . 
-    b d d c d 5 5 b 5 5 5 5 5 5 b . 
-    c d d d c c b 5 5 5 5 5 5 5 b . 
-    c b d d d d d 5 5 5 5 5 5 5 b . 
-    . c d d d d d d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    mySprite.setVelocity(0, 0)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . e e e . . . . . . . 
+    . . . . . . e e e . . . . . . . 
+    . . . . . . e d d . . . . . . . 
+    . . . . . . d d d . . d . . . . 
+    . . . . d f f f f f d d . . . . 
+    . . . . d d d f f f d d . . . . 
+    . . . . . . f f f . . . . . . . 
+    . . . . . . 8 8 8 . . . . . . . 
+    . . . . . . 8 . 8 . . . . . . . 
+    . . . . . . 8 . 8 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`level1`)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 97))
+controller.moveSprite(mySprite, 50, 50)
+mySprite.setScale(2, ScaleAnchor.Middle)
+forever(function () {
+    mySprite.setVelocity(0, 50)
+})
